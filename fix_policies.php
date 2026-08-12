1068027382f017fe96fe7ef03425e5d64d2bf3c0
Ankita <210303105155@paruluniversity.ac.in>
2026-08-12 23:36:23 +0530

    removed unwanted files

--- fix_policies.php
+++
@@ -1,7 +0,0 @@
-<?php
-foreach(glob(__DIR__ . '/app/Policies/*.php') as $file) {
-    $content = file_get_contents($file);
-    $content = str_replace('(Admin $user', '(User $user', $content);
-    file_put_contents($file, $content);
-}
-echo "Done";
