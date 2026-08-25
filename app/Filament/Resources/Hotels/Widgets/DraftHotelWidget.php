e7e82893e7d1e3b01cf15000d667918ce84d5ce6
Jyoti Pal <amjyo9611@gmail.com>
2026-08-25 17:31:25 +0530

    Replace native browser confirm with Filament modal popup for Discard Draft on Hotels

--- app/Filament/Resources/Hotels/Widgets/DraftHotelWidget.php
+++ app/Filament/Resources/Hotels/Widgets/DraftHotelWidget.php
@@ -2,10 +2,18 @@
 
 namespace App\Filament\Resources\Hotels\Widgets;
 
+use Filament\Actions\Action;
+use Filament\Actions\Concerns\InteractsWithActions;
+use Filament\Actions\Contracts\HasActions;
+use Filament\Schemas\Concerns\InteractsWithSchemas;
+use Filament\Schemas\Contracts\HasSchemas;
 use Filament\Widgets\Widget;
 
-class DraftHotelWidget extends Widget
+class DraftHotelWidget extends Widget implements HasActions, HasSchemas
 {
+    use InteractsWithActions;
+    use InteractsWithSchemas;
+
     protected string $view = 'filament.resources.hotels.widgets.draft-hotel-widget';
 
     public ?array $draft = null;
@@ -15,9 +23,21 @@ class DraftHotelWidget extends Widget
         $this->draft = cache()->get('draft_hotel_' . auth()->id());
     }
 
-    public function clearDraft(): void
+    public function discardDraftAction(): Action
     {
-        cache()->forget('draft_hotel_' . auth()->id());
-        $this->draft = null;
+        return Action::make('discardDraft')
+            ->label('Discard Draft')
+            ->color('danger')
+            ->icon('heroicon-o-trash')
+            ->requiresConfirmation()
+            ->modalHeading('Discard Hotel Draft?')
+            ->modalDescription('Are you sure you want to discard this draft? All unsaved data will be permanently lost.')
+            ->modalSubmitActionLabel('Yes, Discard Draft')
+            ->modalIcon('heroicon-o-exclamation-triangle')
+            ->modalIconColor('danger')
+            ->action(function () {
+                cache()->forget('draft_hotel_' . auth()->id());
+                $this->draft = null;
+            });
     }
 }
